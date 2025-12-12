0d641cabcf2f664b588467ea9a014b9ca933a125
renata <renata_castillo_@outlook.com>
2025-12-12 13:12:52 -0600

    click counter function

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:app1/presentation/screens/counter_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,9 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(body: Center(child: Text('Hola Mundo'))),
+      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.teal),
+      home: const CounterScreen(),
     );
   }
 }
--- lib/presentation/screens/counter_screen.dart
+++ lib/presentation/screens/counter_screen.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends StatefulWidget {
   const CounterScreen({super.key});
 
   @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+//state: maneja el estado de counterscreen
+class _CounterScreenState extends State<CounterScreen> {
+  int clickCounter = 0;
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Hola Mundo')));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Rick & Morty Enciclopedia'),
+        leading: IconButton(onPressed: () {}, icon: Icon(Icons.science)),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '$clickCounter',
+              style: TextStyle(fontSize: 160, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'Click${clickCounter == 1 ? '' : 's'}',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            Text('Desc'),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          clickCounter++;
+          setState(() {});
+        },
+        child: const Icon(Icons.plus_one),
+      ),
+    );
   }
 }
